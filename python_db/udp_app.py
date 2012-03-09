2b4c81b877f04deea3a2038ce8507023af6d627f
The0s <theodosis8@hotmail.com>
2012-03-09 17:33:02 +0000

    runs

---
+++ python_db/udp_app.py
@@ -0,0 +1,36 @@
+import sys 
+sys.path.append("/home/the0project/Desktop/ProjectSimulator/python_db")
+sys.path.append("C:\Users\tg108.ee-ms6062\Desktop\Project\python_db")
+from db_model import * 
+from server import *
+from UDPtoRabbitmq import *
+
+if __name__ == "__main__":
+	#UDP_IP = "192.168.1.98"
+	UDP_IP = "155.198.117.28"
+	UDP_PORT= 7011
+	sock = Server(UDP_IP, UDP_PORT)
+
+	if (len(sys.argv) > 2):
+		UDP_IP = argv[1]
+		UDP_PORT = argv[2]
+
+	i = 1
+	client = RabbitmqClient()
+
+	while True:
+		try:
+			#data structure = (Speed;brakes;gas;clutch;gear;distance;time)
+			data, addr = sock.receive()
+			if i == 1 :
+				print "Received Message From: ", addr[0],":",addr[1]
+			print i
+			i+=1
+			#print data
+			#print len(data)
+			client.send(data)
+ 		except (KeyboardInterrupt, SystemExit):
+        		raise
+		
+		
+
